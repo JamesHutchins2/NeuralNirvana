89dfa16ed866c7253a92000e8c1953c1eaf58b19
james hutchins <jhutch63@uwo.ca>
2023-12-04 13:40:33 -0500

    remove old function

--- pipeline/clean.py
+++
@@ -1,9 +0,0 @@
-
-
-# we will want to clean with a band pass filter
-
-# than we will want to normalize using the mean and standard deviation so that average is zero, and standard deviation is 1
-
-
-# we may also want to extract statistical features from the data, such as the mean, standard deviation, and variance or spectral density, or even wavelet coefficents
-
