f23f503e70e8b3bb62a05c1c07d02fd4b26dfb51
james hutchins <jhutch63@uwo.ca>
2023-12-11 07:31:45 -0500

    integrated positional encodings

--- mne_method/positional_encoding/AbsolutePositionalEncoding.py
+++ mne_method/positional_encoding/AbsolutePositionalEncoding.py
@@ -20,17 +20,22 @@ class tAPE(nn.Module):
         max_len: the max. length of the incoming sequence (default=1024).
     """
 
-    def __init__(self, d_model, dropout=0.1, max_len=1024, scale_factor=1.0):
+    def __init__(self, d_model=63, dropout=0.1, max_len=1028, scale_factor=1.0):
         super(tAPE, self).__init__()
         self.dropout = nn.Dropout(p=dropout)
         pe = torch.zeros(max_len, d_model)  # positional encoding
         position = torch.arange(0, max_len, dtype=torch.float).unsqueeze(1)
-        div_term = torch.exp(torch.arange(0, d_model, 2).float() * (-math.log(10000.0) / d_model))
+        
+        # Corrected calculation of div_term
+        div_term = torch.exp(torch.arange(0, d_model, dtype=torch.float) * (-math.log(10000.0) / d_model))
 
-        pe[:, 0::2] = torch.sin((position * div_term)*(d_model/max_len))
-        pe[:, 1::2] = torch.cos((position * div_term)*(d_model/max_len))
+        # Apply sine and cosine to the entire d_model
+        pe[:, 0::2] = torch.sin(position / div_term[0::2])
+        pe[:, 1::2] = torch.cos(position / div_term[1::2])
+
         pe = scale_factor * pe.unsqueeze(0)
         self.register_buffer('pe', pe)  # this stores the variable in the state_dict (used for non-trainable variables)
+
 
     def forward(self, x):
         r"""Inputs of forward function
